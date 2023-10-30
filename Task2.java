c29a03ce343d03aa7fec802be294b4a1ef9603af
Abhang Paturkar <abhangpaturkar0709@gmail.com>
2023-10-30 23:17:56 +0530

    Task2 Initiate

---
+++ Task2.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Task2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the number of subjects: ");
+        int totalSubjects = scanner.nextInt();
+        int totalMarks = 0;
+
+        for (int i = 1; i <= totalSubjects; i++) {
+            System.out.print("Enter marks obtained in subject " + i + " (out of 100): ");
+            int marks = scanner.nextInt();
+            totalMarks += marks;
+        }
+
+        double averagePercentage = (double) totalMarks / totalSubjects;
+
+        String grade;
+        if (averagePercentage >= 90) {
+            grade = "A+";
+        } else if (averagePercentage >= 80) {
+            grade = "A";
+        } else if (averagePercentage >= 70) {
+            grade = "B";
+        } else if (averagePercentage >= 60) {
+            grade = "C";
+        } else if (averagePercentage >= 50) {
+            grade = "D";
+        } else {
+            grade = "F";
+        }
+
+        System.out.println("Total Marks: " + totalMarks);
+        System.out.println("Average Percentage: " + averagePercentage + "%");
+        System.out.println("Grade: " + grade);
+
+        scanner.close();
+    }
+}
