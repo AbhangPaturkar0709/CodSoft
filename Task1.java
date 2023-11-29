8ae048567df24dedf4906b6531b32e96dfcaa9d5
Abhang Paturkar <abhangpaturkar0709@gmail.com>
2023-11-29 16:56:38 +0530

    Task1 Completed

--- Task1.java
+++ Task1.java
@@ -1,5 +1,5 @@
 /*
- * Task 1
+ *  Task 1
  * 1. Generate a random number within a specified range, such as 1 to 100.
  * 2. Prompt the user to enter their guess for the generated number.
  * 3. Compare the user's guess with the generated number and provide feedback on whether 
