7c096cfcac8d193991590efce506c56c99ac1b94
Abhang Paturkar <abhangpaturkar0709@gmail.com>
2023-11-29 17:00:49 +0530

    Task5 Completed

--- Task5.java
+++ Task5.java
@@ -14,22 +14,17 @@ public class Task5 {
         Scanner scanner = new Scanner(System.in);
 
         try {
-            // Register JDBC driver
             Class.forName(JDBC_DRIVER);
 
-            // Open a connection
             System.out.println("Connecting to database...");
             conn = DriverManager.getConnection(DB_URL, USER, PASS);
 
-            // Create statement
             stmt = conn.createStatement();
 
-            // Display menu and perform actions until the user chooses to exit
             char choice;
             do {
                 displayMenu();
 
-                // Get user choice
                 int userChoice = getUserChoice();
 
                 switch (userChoice) {
@@ -46,7 +41,6 @@ public class Task5 {
                         System.out.println("Invalid choice. Please choose a valid option.");
                 }
 
-                // Ask user if they want to repeat the action
                 System.out.print("Do you want to perform another action? (yes/no): ");
                 choice = scanner.next().toLowerCase().charAt(0);
             } while (choice == 'y');
@@ -54,20 +48,18 @@ public class Task5 {
         } catch (SQLException | ClassNotFoundException se) {
             handleException(se);
         } finally {
-            // Close resources
             try {
                 if (stmt != null)
                     stmt.close();
                 if (conn != null)
                     conn.close();
-                scanner.close(); // Close the scanner
+                scanner.close();
             } catch (SQLException se) {
                 handleException(se);
             }
         }
     }
 
-    // Display menu
     private static void displayMenu() {
         System.out.println("\nStudent Course Registration System Menu:");
         System.out.println("1. Display Course Listing");
@@ -75,7 +67,6 @@ public class Task5 {
         System.out.println("3. Drop Course for Student\n");
     }
 
-    // Get user choice
     private static int getUserChoice() {
         System.out.print("Enter your choice: ");
         try {
@@ -85,17 +76,14 @@ public class Task5 {
         }
     }
 
-    // Display Course Listing
     private static void displayCourseListing(Statement stmt) {
         try {
             String sql = "SELECT * FROM Course";
             ResultSet rs = stmt.executeQuery(sql);
 
-            // Check if there are any courses available
             if (!rs.isBeforeFirst()) {
                 System.out.println("No courses available in the database.");
             } else {
-                // Display courses in tabular form
                 System.out.println("Course Listing:");
                 System.out.println("-------------------------------------------------------------");
                 System.out.printf("%-15s %-40s %-30s %-10s\n", "Code", "Title", "Description", "Capacity");
@@ -116,12 +104,11 @@ public class Task5 {
         }
     }
 
-    // Register Student for Course with user inputs
     private static void registerStudentForCourse(Connection conn, Statement stmt, Scanner scanner) {
         try {
             System.out.print("Enter student ID: ");
             int studentId = scanner.nextInt();
-            scanner.nextLine(); // Consume the newline character
+            scanner.nextLine();
             System.out.print("Enter course code for registration: ");
             String courseCode = scanner.nextLine();
 
@@ -140,12 +127,11 @@ public class Task5 {
         }
     }
 
-    // Drop Course for Student with user inputs
     private static void dropCourseForStudent(Connection conn, Statement stmt, Scanner scanner) {
         try {
             System.out.print("Enter student ID: ");
             int studentId = scanner.nextInt();
-            scanner.nextLine(); // Consume the newline character
+            scanner.nextLine();
             System.out.print("Enter course code for removal: ");
             String courseCode = scanner.nextLine();
 
@@ -163,7 +149,6 @@ public class Task5 {
         }
     }
 
-    // Handle exceptions and display appropriate messages
     private static void handleException(Exception e) {
         if (e instanceof SQLException) {
             System.out.println("SQL Error: " + e.getMessage());
